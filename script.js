aa60ea6b153ba7bae75e0023fdaad3323dcc9a80
ActuallyIsJesse <jesse.al.lord@gmail.com>
2023-06-17 17:00:35 -0400

    Add book class

---
+++ script.js
@@ -0,0 +1,10 @@
+let library = [];
+
+class Book {
+    constructor(title, author, year, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.haveRead = haveRead;
+    }
+}
