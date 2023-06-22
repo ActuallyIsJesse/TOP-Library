7a005b14e9078e311e892b36641fb4739a953e8e
ActuallyIsJesse <jesse.al.lord@gmail.com>
2023-06-22 16:50:49 -0400

    Add modal toggle

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let library = [];
+const library = [];
+const newBook = document.querySelector(".new-book-button");
+const libraryContent = document.querySelector(".library-content");
+const modal = document.querySelector(".modal");
 
 class Book {
     constructor(title, author, year, haveRead) {
@@ -8,3 +11,32 @@ class Book {
         this.haveRead = haveRead;
     }
 }
+
+function updateLibrary() {
+    while(library.firstChild){
+        library.removeChild(library.firstChild);
+    }
+    for (i = 0; i < library.length; i++) {
+        console.log("");
+    }
+}
+
+function createBookDom(book) {
+    const wrapper = document.createElement('div');
+    const title = document.createElement('h1');
+    const author = document.createElement('p');
+    const year = document.createElement ('p');
+    const haveRead = document.createElement ('p');
+}
+
+function modalToggle() {
+    if (window.getComputedStyle(modal).getPropertyValue('display') === 'none') {
+        modal.style.setProperty('display', 'flex');
+    } else {
+        modal.style.setProperty('display', 'none')
+    }
+}
+
+newBook.addEventListener('click', () => {
+    modalToggle()
+})
