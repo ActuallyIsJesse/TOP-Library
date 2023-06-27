35206bc76acbe242e9817ba9111591dc7f06c8b0
ActuallyIsJesse <jesse.al.lord@gmail.com>
2023-06-26 23:34:45 -0400

    Add "add to library" function

--- script.js
+++ script.js
@@ -5,66 +5,91 @@ const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal-close-button");
 const createBookForm = document.querySelector(".create-book-form");
 
+/*const tempBook1 = {
+  title: "The Hobbit",
+  author: "Tolkien",
+  year: "1937",
+  haveRead: true,
+};
+
+drawBook(tempBook1);
+*/
+
 class Book {
-    constructor(title, author, year, haveRead) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.haveRead = haveRead;
-    }
+  constructor(title, author, year, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.haveRead = haveRead;
+  }
 }
 
 function addBook(form) {
-    const bookData = new FormData(form);
-    library.unshift(Object.fromEntries(bookData));
+  const bookData = new FormData(form);
+  let newBook = Object.fromEntries(bookData);
+  if (newBook.haveRead === "on") {
+    newBook.haveRead = true;
+  } else {
+    newBook.haveRead = false;
+  }
+  library.unshift(newBook);
+  drawBook(library[0]);
 }
 
 function updateLibrary() {
-    while(library.firstChild){
-        library.removeChild(library.firstChild);
-    }
-    for (i = 0; i < library.length; i++) {
-        console.log("");
-    }
+  while (library.firstChild) {
+    library.removeChild(library.firstChild);
+  }
+  for (i = 0; i < library.length; i++) {
+    drawBook(library[i]);
+  }
 }
 
 function drawBook(book) {
-    const wrapper = document.createElement('div');
-    const title = document.createElement('h1');
-    title.innerText = `${book.title}`;
-    wrapper.append(title);
-    const author = document.createElement('p');
-    author.innerText = `${book.author}`;
-    wrapper.append(author);
-    const year = document.createElement ('p');
-    year.innerText = `${book.year}`;
-    wrapper.append(year);
-    const haveRead = document.createElement ('p');
-    haveRead.innerText = `${book.haveRead}`;
-    wrapper.append(haveRead);
-    libraryContent.append(wrapper)
+  const wrapper = document.createElement("div");
+  wrapper.dataset.index = 1;
+  wrapper.classList.add("card");
+  const title = document.createElement("h1");
+  title.innerText = `${book.title}`;
+  wrapper.append(title);
+  const author = document.createElement("p");
+  author.innerText = `${book.author}`;
+  wrapper.append(author);
+  const year = document.createElement("p");
+  year.innerText = `${book.year}`;
+  wrapper.append(year);
+  const haveRead = document.createElement("input");
+  haveRead.type = "checkbox";
+  if (book.haveRead) {
+    haveRead.checked = true;
+  } else {
+    haveRead.checked = false;
+  }
+  haveRead.classList.add("read-checkmark")
+  wrapper.append(haveRead);
+  libraryContent.append(wrapper);
 }
 
 function modalToggle() {
-    if (window.getComputedStyle(modal).getPropertyValue('display') === 'none') {
-        modal.style.setProperty('display', 'flex');
-    } else {
-        modal.style.setProperty('display', 'none');
-    }
-    createBookForm.reset();
+  if (window.getComputedStyle(modal).getPropertyValue("display") === "none") {
+    modal.style.setProperty("display", "flex");
+  } else {
+    modal.style.setProperty("display", "none");
+  }
+  createBookForm.reset();
 }
 
-newBook.addEventListener('click', () => {
-    modalToggle();
-})
+newBook.addEventListener("click", () => {
+  modalToggle();
+});
 
-modalClose.addEventListener('click', () => {
-    modalToggle();
-})
+modalClose.addEventListener("click", () => {
+  modalToggle();
+});
 
-createBookForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    addBook(event.target);
-    event.target.reset();
-    modalToggle();
-})
+createBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBook(event.target);
+  event.target.reset();
+  modalToggle();
+});
